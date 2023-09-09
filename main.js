fb3b1b55bb65425ddb0adfa3470061f28fb7853a
The Fullstack Mechanic <111003107+SirhmVFX@users.noreply.github.com>
2023-09-09 23:39:58 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,22 @@
-function displayUtcTime() {
-  const curentUtcTime = new Date().getTime();
-  return curentUtcTime;
+function fetchAndDisplayUTCTime() {
+    const timeElement = document.querySelector('.time');
+    const utcTime = new Date().toUTCString();
+    timeElement.textContent = `UTC Time: ${utcTime}`;
 }
 
-const currentTimeElement = document.getElementById("current-time");
-
-const currentUTCTimeMillis = displayUtcTime();
-currentTimeElement.textContent = `Current UTC Time in Milliseconds: ${currentUTCTimeMillis}`;
-
-function getCurrentDayOfWeek() {
-  const daysOfWeek = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  const currentDayIndex = new Date().getDay();
-  return daysOfWeek[currentDayIndex];
+function fetchAndDisplayDayOfWeek() {
+    const dayOfWeekElement = document.querySelector('.dayOfTheWeek');
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const currentDayOfWeek = new Date().getDay();
+    const dayName = daysOfWeek[currentDayOfWeek];
+    dayOfWeekElement.textContent = `Day of the Week: ${dayName}`;
 }
 
-const currentDayElement = document.getElementById("current-day");
-const currentDay = getCurrentDayOfWeek();
-currentDayElement.textContent = `Current Day of the Week: ${currentDay}`;
+function updateDateTime() {
+    fetchAndDisplayUTCTime();
+    fetchAndDisplayDayOfWeek();
+}
+
+updateDateTime();
+
+setInterval(updateDateTime, 1000);
